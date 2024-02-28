934c14ba5f981a463544d6400ca9a16deea5c1f1
nomadic-dev <nomadicdev.io@gmail.com>
2024-02-28 08:09:40 +0400

    info section added

--- src/components/sections/NEPInterviewIntroSection.jsx
+++ src/components/sections/NEPInterviewIntroSection.jsx
@@ -27,12 +27,9 @@ const FirstStep = ()=> {
         >
             <div className="image_holder">
                 <div className="holder_">
-                    {
-                        false &&
-                        <div className="img_">
-                            <img src="/interview-info.svg" />
-                        </div>
-                    }
+                    <div className="img_">
+                        <img src="/interview-info.svg" />
+                    </div>
                 </div>
 
             </div>
@@ -54,13 +51,22 @@ const SecondeStep = ()=> {
             exit={{ opacity: 0, x: -50 }}
             transition={{ duration: 0.3, delay: 0.2 }}
         >
-            <div className="image_">
-            
+            <div className="image_holder">
+                <div className="holder_">
+                    <div className="img_">
+                        <img src="/interview-info.svg" />
+                    </div>
+                </div>
             </div>
+
             <div className="content_">
-                <h3>Hi <strong>Alan Sha Salim</strong>, the next part of the application process is an AI interview.</h3>
-                <p>These questions will gauge your thinking about the transformative projects you have worked on during your career.</p>
-                <p>Please take some time to consider the questions on the next page before proceeding with the AI interview. Your answers will be judged on how you demonstrate your strategic thinking, adaptability to challenges, pro-active mindset, perseverance, future-ready focus, and ability to empathize with others. </p>
+                <h3>Interview <strong>Questions</strong></h3>
+                <ul>
+                    <li>Please describe the transformative project you worked on, its impact and your exact role.</li>
+                    <li>Please provide up to 3 examples of important disruptive innovations and mega-trends in tech, socio and geo-political fields and their implications for your organization.</li>
+                    <li>Please provide one specific example of how you fostered and managed creativity and innovation in your team.</li>
+                    <li>Can you share one specific example of how you successfully managed and implemented strategic change in your organization or sector?</li>
+                </ul>
             </div>
         </motion.div>
     )
@@ -78,7 +84,7 @@ const NEPInterviewIntroSection = () => {
         <NEPApplicationSteps />
 
         <FirstStep />
-
+        <SecondeStep />
         <ActionBar onStepClick={onStepClick}/>
 
 
